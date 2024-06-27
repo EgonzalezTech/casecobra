c6674686692175bd0e6396c39693360fe3719dfd
egonzalezTech <edgonzalez.mendoza@gmail.com>
2024-06-27 18:36:40 -0400

    Primer commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,17 +17,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en">
       <body className={recursive.className}>
-        {" "}
         <Navbar />
+
         <main className="flex grainy-light flex-col min-h-[calc(100vh-3.5rem-1px)]">
           <div className="flex-1 flex flex-col h-full">
             <Providers>{children}</Providers>
           </div>
-
           <Footer />
         </main>
+
         <Toaster />
       </body>
     </html>
